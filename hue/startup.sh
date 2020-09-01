b6001b4d811542806ee7ee6a2fa7a0802afa5a38
MattToczek <toczek.matt@gmail.com>
2020-09-01 16:23:04 +0100

    adding local hue.ini, amending startup.sh adding user vars to hue.ini and set s3fs variables

--- hue/startup.sh
+++ hue/startup.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 ./build/env/bin/hue migrate
-./build/env/bin/supervisor
 
-CREDENTIALS=$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
-ACCESS_KEY_ID=$(echo $CREDENTIALS | jq '.AccessKeyId')
-SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq '.SecretAccessKey')
-TOKEN=$(echo $CREDENTIALS | jq '.Token')
-REGION=$( [[ ! -z "$AWS_REGION" ]] && echo "$AWS_REGION" || echo "$AWS_DEFAULT_REGION" )
-sed -i '/[[[default]]]/s/\s*##//g' /usr/share/hue/desktop/conf/hue.ini
-sed -i 's/## access_key_id=/access_key_id=\'${ACCESS_KEY_ID}'/g' /usr/share/hue/desktop/conf/hue.ini
-sed -i 's/## secret_access_key=/secret_access_key=\'${SECRET_ACCESS_KEY}'/g' /usr/share/hue/desktop/conf/hue.ini
-sed -i 's/## security_token=/security_token=\'${TOKEN}'/g' /usr/share/hue/desktop/conf/hue.ini
-sed -i 's/## region=us-east-1/region=\'${REGION}'/g' /usr/share/hue/desktop/conf/hue.ini
+HOME=/usr/share/hue
+MNT_POINT=$HOME/s3fs
+
+wget -O $HOME/raw.txt 169.254.170.2"$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"
+
+ACCESS_KEY_ID=$(awk '{gsub(/.*"AccessKeyId":"|".*/,"");print}' $HOME/raw.txt)
+SECRET_ACCESS_KEY=$(awk '{gsub(/.*"SecretAccessKey":"|".*/,"");print}' $HOME/raw.txt)
+
+rm $HOME/raw.txt
+
+cat "$ACCESS_KEY_ID:$SECRET_ACCESS_KEY" > passwd && chmod 600 passwd
+
+s3fs "$S3_BUCKET" "$MNT_POINT"
+
 sed -i 's*## livy_server_url=http://localhost:8998*livy_server_url=http://\'${EMR_HOST_NAME}':8998*g' /usr/share/hue/desktop/conf/hue.ini
-sed -i '/## security_enabled=false/s/\s*##//g' /usr/share/hue/desktop/conf/hue.ini
+sed -i 's*## webhdfs_url=http://localhost*webhdfs_url=http://\'${EMR_HOST_NAME}'*g' /usr/share/hue/desktop/conf/hue.ini
+sed -i 's*## default_user=hue*default_user=\'${USER}'*g' /usr/share/hue/desktop/conf/hue.ini
+sed -i 's*## proxy_user=hue*proxy_user=\'${USER}'*g' /usr/share/hue/desktop/conf/hue.ini
+
+./build/env/bin/supervisor
