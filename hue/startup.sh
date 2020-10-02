5021f2a4ba0b3f56c6569551e07543f016bdea11
Ian Utley <ian.utley@infinityworks.com>
2020-10-02 11:51:50 +0100

    Add a default superuser (#37)
    * Add a default superuser
    * Change thriftserver port

--- hue/startup.sh
+++ hue/startup.sh
@@ -3,5 +3,6 @@
 cat /tmp/hue-overrides.ini | envsubst > ./desktop/conf/hue-overrides.ini
 
 ./build/env/bin/hue migrate
+./build/env/bin/hue createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME:-hue} --email ${DJANGO_SUPERUSER_EMAIL:-hue@example.org}
 ./build/env/bin/supervisor
 
