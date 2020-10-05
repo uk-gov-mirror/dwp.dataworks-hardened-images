f5bde8cba1fc17df999b28964020c34b29694dfa
Ian Utley <ian.utley@infinityworks.com>
2020-10-05 10:35:31 +0100

    Deploy as HTTPS (#39)

--- hue/startup.sh
+++ hue/startup.sh
@@ -2,6 +2,8 @@
 
 cat /tmp/hue-overrides.ini | envsubst > ./desktop/conf/hue-overrides.ini
 
+/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /usr/share/hue/key.pem -out /usr/share/hue/cert.pem -days 30 -nodes -subj '/CN=hue'
+
 ./build/env/bin/hue migrate
 ./build/env/bin/hue createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME:-hue} --email ${DJANGO_SUPERUSER_EMAIL:-hue@example.org}
 ./build/env/bin/supervisor
