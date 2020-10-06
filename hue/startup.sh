53498bf3d9980983ab0cf62d1a21dc6c22d5838e
Ian Utley <ian.utley@infinityworks.com>
2020-10-06 14:00:29 +0100

    Use MariaDB for hue database (#42)

--- hue/startup.sh
+++ hue/startup.sh
@@ -4,6 +4,19 @@ cat /tmp/hue-overrides.ini | envsubst > ./desktop/conf/hue-overrides.ini
 
 /usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /usr/share/hue/key.pem -out /usr/share/hue/cert.pem -days 30 -nodes -subj '/CN=hue'
 
+mariadb-install-db --user=hue --datadir=/usr/share/hue/data --skip-test-db --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880
+echo Preparing to start DB server
+sleep 5
+mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/usr/share/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
+echo Preparing to reset DB password
+sleep 5
+mariadb --socket=/tmp/maria.sock  << !EOF
+    CREATE DATABASE hue COLLATE = 'utf8_general_ci';
+    GRANT ALL ON hue.* TO 'hue'@'%' IDENTIFIED BY 'notsecret';
+    SELECT * FROM INFORMATION_SCHEMA.SCHEMATA;
+    QUIT
+!EOF
+
 ./build/env/bin/hue migrate
 ./build/env/bin/hue createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME:-hue} --email ${DJANGO_SUPERUSER_EMAIL:-hue@example.org}
 ./build/env/bin/supervisor
