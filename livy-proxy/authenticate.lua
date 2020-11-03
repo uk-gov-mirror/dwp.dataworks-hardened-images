974ca48a0360120bfe26e797105d57005e91b03e
draagc-iw <55280269+draagc-iw@users.noreply.github.com>
2020-11-03 16:27:15 +0000

    Add support for JWT tokens in Authorization header (#56)

--- livy-proxy/authenticate.lua
+++ livy-proxy/authenticate.lua
@@ -1,3 +1,8 @@
-local jwt = check_jwt(ngx.var.arg_token)
+local req_headers = ngx.req.get_headers()
+local token = (req_headers["Authorization"]
+            and req_headers["Authorization"]
+            or ngx.var.arg_token)
+
+local jwt = check_jwt(token)
 substitute_proxy_user(jwt)
 remove_token_query_param()
